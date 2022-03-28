39627cbd9e373a982bdac095df3af039709a750b
MUHAMMED ASHRAF <102135237+MonuuzXd@users.noreply.github.com>
2022-03-28 08:17:24 +0530

    Update assistant.py

--- handlers/assistant.py
+++ handlers/assistant.py
@@ -22,7 +22,7 @@ async def join_chat(c: Client, m: Message):
             (await user.get_me()).id,
             can_manage_voice_chats=True
         )
-        return await user.send_message(chat_id, "✅ userbot entered chat")
+        return await user.send_message(chat_id, "💞 singer വന്നു ഗയ്‌സ് 😂")
     except UserAlreadyParticipant:
         admin = await m.chat.get_member((await user.get_me()).id)
         if not admin.can_manage_voice_chats:
@@ -30,5 +30,5 @@ async def join_chat(c: Client, m: Message):
                 (await user.get_me()).id,
                 can_manage_voice_chats=True
             )
-            return await user.send_message(chat_id, "✅ userbot already in chat")
-        return await user.send_message(chat_id, "✅ userbot already in chat")
+            return await user.send_message(chat_id, "💞 singer already ഇവിടെ ഉണ്ട് 😒")
+        return await user.send_message(chat_id, "💞 singer already ഞാൻ ഉണ്ട് ഇവിടെ 🤭")
